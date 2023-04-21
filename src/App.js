f54a76450dc4e2f2a3792060fced3773e6fe8da7
Scott Thompson <sthompson1@colgate.edu>
2023-04-21 14:50:41 -0700

    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Original } from './pages/home/components/Original';
+import {useEffect, useState} from 'react';
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCounter((count) => counter + 1);
+    }, 1000);
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Original name={counter}></Original>
+    <h1>{counter}</h1>
+    </>
   );
 }
 
---
+++ src/pages/home/components/Original.js
@@ -0,0 +1,6 @@
+export function Original(props){
+    return <>
+    <h1>Hello {props.name}</h1>
+    </>;
+    
+}
